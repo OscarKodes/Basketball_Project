3689e79aa748fee700c6f43e379fdac3f9dba30c
Oscar Ko <OscarKoEmail@gmail.com>
2021-11-26 19:46:16 -0500

    Add basic trendline scale, add drop down variable selection

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * 0.7,
+  height = window.innerWidth * 0.7,
+  margin = {
+    top: 50,
+    bottom: 50,
+    left: 50,
+    right: 50
+  },
+  radius = 3;
 
-
-// these variables allow us to access anything we manipulate in init() but need access to in draw().
-// All these variables are empty before we assign something to them.
-// let svg;
-// let xScale;
-// let yScale; ...
+let svg;
+let xScale;
+let yScale;
+let xAxis, yAxis;
+let xAxisGroup, yAxisGroup;
+let bothPlayersData;
 
 /* APPLICATION STATE */
 let state = {
     data: [],
-    selection: "All", // + YOUR FILTER SELECTION
+    variable: "Games_Played",
+    player1: "Aaron Brooks",
+    player2: "Al-Farouq Aminu"
   };
   
   /* LOAD DATA */
@@ -23,33 +30,100 @@ let state = {
     // use custom initializer to reformat the data the way we want it
     // ref: https://github.com/d3/d3-fetch#dsv
     return {
-      player: d.Player,
-      year: +d.Season.split(" - ")[1],
+      Player: d.Player,
+      Year: +d.Season.split(" - ")[1],
+      Games_Played: +d.Games_Played,
+      Minutes_Played: +d.Minutes_Played,
+      Field_Goals_Made: +d.Field_Goals_Made,
+      Field_Goal_Attempts: +d.Field_Goal_Attempts,
+      Three_Pointers_Made: +d.Three_Pointers_Made,
+      Three_Pointers_Attempts: +d.Three_Pointers_Attempts,
+      Free_Throws_Made: +d.Free_Throws_Made,
+      Free_Throw_Attempts: +d.Free_Throw_Attempts,
+      Turnovers: +d.Turnovers,
+      Personal_Fouls: +d.Personal_Fouls,
+      Offensive_Rebounds: +d.Offensive_Rebounds,
+      Defensive_Rebounds: +d.Defensive_Rebounds,
+      All_Rebounds: +d.All_Rebounds,
+      Assists: +d.Assists,
+      Steals: +d.Steals,
+      Blocks: +d.Blocks,
+      Total_Points_Made: +d.Total_Points_Made,
+      Salary: +d.Salary,
+      Cost_Per_Point: +d.Cost_Per_Point
     }
   })
     .then(data => {
       console.log("loaded data:", data);
-      console.log(data[0]);
       state.data = data;
+ 
       init();
     });
   
   /* INITIALIZING FUNCTION */
   // this will be run *one time* when the data finishes loading in
   function init() {
+
+    bothPlayersData = state.data.
+      filter(d => [state.player1, state.player2].includes(d.Player));
+
     // + SCALES
-  
-  
-    // + AXES
-  
+    xScale = d3.scaleLinear()
+      .domain(d3.extent(bothPlayersData, d => d.Year))
+      .range([margin.left, width - margin.right]);
+
+    yScale = d3.scaleLinear()
+      .domain(d3.extent(bothPlayersData, d => d.Games_Played))
+      .range([height - margin.bottom, margin.top])
+      .nice();
   
     // + UI ELEMENT SETUP
-  
+    const selectDropMenu = d3.select("#compare-trendlines-variable");
+    const selectUpdateBtn = d3.select("#update-trendlines-btn");
+
+    const varNames = Object.keys(state.data[0]).slice(2);
+
+    selectDropMenu.selectAll("option")
+      .data(varNames)
+      .join("option")
+      .attr("attr", d => d)
+      .attr("value", d => d)
+      .text(d => d.replaceAll("_", " "));
+
+    selectUpdateBtn.on("click", e => {
+
+      const DOM_menu = document.getElementById("compare-trendlines-variable");
+      const selectedOption = DOM_menu.options[DOM_menu.selectedIndex].value;
+
+      state.variable = selectedOption;
+    });
   
     // + CREATE SVG ELEMENT
-    
+    svg = d3.select("#vis-trendlines svg")
+        .attr("width", width)
+        .attr("height", height)
+        .style("background-color", "lavender")
+        .attr("opacity", "0.5")
   
     // + CALL AXES
+    xAxisGroup = svg.select(".x-axis")
+      .attr("transform", `translate(${0}, ${height - margin.bottom})`)
+      .call(d3.axisBottom(xScale));
+
+    yAxisGroup = svg.select(".y-axis")
+      .attr("transform", `translate(${margin.right}, ${0})`)
+      .call(d3.axisLeft(yScale));
+
+    // AXIS LABELS
+    svg.select(".x-label")
+      .attr("transform", `translate(${width / 2}, ${height - margin.bottom * .2})`)
+      .text("Year")
+
+    svg.select(".y-label")
+      .attr("class", 'yLabel')
+      .attr("transform", `translate(${18}, ${height / 2})`)
+      .attr("writing-mode", 'vertical-rl')
+      .text("Games Played")
   
   
     draw(); // calls the draw function
@@ -58,20 +132,44 @@ let state = {
   /* DRAW FUNCTION */
   // we call this every time there is an update to the data/state
   function draw() {
-    // + FILTER DATA BASED ON STATE
-    const filteredData = state.data
-      // .filter(d => d.country === state.selection)
+  // + FILTER DATA BASED ON STATE
+  const filteredData = state.data.
+    filter(d => [state.player1, state.player2].includes(d.Player));
+
+
+  // // + UPDATE SCALE(S), if needed
+  // yScale.domain([0, d3.max(filteredData, d => d.population)])
+
+  // // + UPDATE AXIS/AXES, if needed
+  // yAxisGroup
+  //   .transition()
+  //   .duration(1000)
+  //   .call(yAxis.scale(yScale))// need to udpate the scale
+
+  // specify line generator function
+  const lineGen = d3.line()
+    .x(d => xScale(d.Year))
+    .y(d => yScale(d.Games_Played))
+
   
-    // + UPDATE SCALE(S), if needed
-    
-  
-    // + UPDATE AXIS/AXES, if needed
-  
-  
-    // UPDATE LINE GENERATOR FUNCTION
-  
-  
-    // + DRAW LINE AND/OR AREA
-    
-  
+  const player1data = bothPlayersData.filter(d => d.Player === state.player1);
+  const player2data = bothPlayersData.filter(d => d.Player === state.player2);
+
+  // + DRAW LINE AND/OR AREA
+  svg.selectAll(".line1")
+    .data([player1data]) // data needs to take an []
+    .join("path")
+    .attr("class", 'line1')
+    .attr("fill", "none")
+    .attr("stroke", "red")
+    .attr("d", d => lineGen(d));
+
+  // + DRAW LINE AND/OR AREA
+  svg.selectAll(".line2")
+    .data([player2data]) // data needs to take an []
+    .join("path")
+    .attr("class", 'line2')
+    .attr("fill", "none")
+    .attr("stroke", "blue")
+    .attr("d", d => lineGen(d));
   }
